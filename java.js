4ff3a54b0d6d810d380ee9c6a753134700d56fc6
Sergio Reyes <rafitar370@gmail.com>
2021-04-21 18:49:44 -0500

    Primera versión

---
+++ java.js
@@ -0,0 +1,7 @@
+alert("¡Bienvenidas mujeres emprendedoras!") /*Mensaje de alerta que da la bienvenida.*/
+
+confirm("¿Deseas saber acerca de Autonomía Económica de las mujeres?"); /*Mensaje de pregunta con botones de confirmación y cancelación.*/
+
+var nombre = prompt("Ingrese su nombre") /*Método para pedir datos al usuario. */
+
+alert("Hola, " + nombre + " te invitamos a ser emprendedor y a desarrollar tu autonomía económica.") /*Alerta para contestar utilizando de variable el dato que se nos proporcionó. */
